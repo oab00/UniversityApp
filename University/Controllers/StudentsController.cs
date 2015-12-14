1b5f5fcfbc8485f86e47bda30a972ad6f0887455
Oab00 <oab93@hotmail.com>
2015-12-14 19:12:43 +0300

    initial commit

---
+++ University/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+using System.Collections.Generic;
+using System.Linq;
+using University.Models;
+
+namespace University.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        Student[] students = new Student[]
+        {
+            new Student { ID = 0, Name = "John Smith", Grade = 80 },
+            new Student { ID = 1, Name = "Jane Roe", Grade = 90 },
+            new Student { ID = 2, Name = "John Doe", Grade = 70 }
+        };
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return students;
+        }
+
+        public IHttpActionResult GetStudent(int id)
+        {
+            var student = students.FirstOrDefault((s) => s.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+    }
+}
---
+++ University/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace University.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+}
